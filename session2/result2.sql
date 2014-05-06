9eb3f2112d9ad91d31001bedb4971e1a21a85802
Bob Rubbens <bobrubbens@gmail.com>
2014-05-06 15:37:35 +0200

    Opgave 2.2 Taak b-1

--- session2/result2.sql
+++ session2/result2.sql
@@ -3,3 +3,12 @@ VALUES
 ('ABC', 'EF', FALSE, 'ABC is een superkey'),
 ('E', 'D', TRUE, 'E is geen superkey'),
 ('D', 'A', TRUE, 'D is geen superkey');
+
+INSERT INTO F1
+VALUES
+('E', 'D', FALSE, 'E is een superkey'),
+
+INSERT INTO F2
+VALUES
+('ABC', 'EF', FALSE, 'ABC is een superkey'),
+('D', 'A', TRUE, 'D is geen superkey');
