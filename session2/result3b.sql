d8ce216126103135b96cd89ebb0298e5fc421af9
Bob Rubbens <bobrubbens@gmail.com>
2014-05-06 16:13:53 +0200

    Foreign key rule

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,7 +1,3 @@
-SELECT p.pid FROM Person p
-WHERE EXISTS (
-	SELECT * FROM Acts a, Movie m
-	WHERE a.pid = p.pid
-	AND m.name = 'Back to the Future'
-	AND m.mid = a.mid
-);
+SELECT a.pid FROM Acts a, Movie m
+WHERE m.name = 'Back to the Future'
+AND m.mid = a.mid;
