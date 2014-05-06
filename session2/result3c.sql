33c9add63f8f584a0e96dfd4e14b436f78809b5e
Bob Rubbens <bobrubbens@gmail.com>
2014-05-06 16:49:25 +0200

    Query gereduceerd want er zat iets dubbelop in

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,10 +1,5 @@
-SELECT p.name FROM Person p
--- WHERE 'schreven voor een film die undirected was'
-WHERE p.pid IN (
-	SELECT w.pid FROM Writes w
-	WHERE p.pid = w.pid
-	-- AND 'de film was undirected'
-	AND w.mid NOT IN (
-		SELECT d.mid FROM Directs d
-	)
+SELECT p.name FROM Person p, Writes w
+WHERE p.pid = w.pid
+AND w.mid NOT IN (
+	SELECT d.mid FROM Directs d
 );
