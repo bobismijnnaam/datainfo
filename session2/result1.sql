ce02d8e0e44f7c9c35a44be2af1872998106469b
Bob Rubbens <bobrubbens@gmail.com>
2014-05-06 15:04:06 +0200

    Opgave 2.1 gemaakt

---
+++ session2/result1.sql
@@ -0,0 +1,12 @@
+INSERT INTO Problem1
+VALUES
+('a', false, 'want meerdere voertuigen kunnen dezelfde capaciteit hebben'),
+('b', true, 'capaciteit is onderdeel van een voertuig (3)'),
+('c', false, 'op een factuur kunnen meerdere pakjes staan, op een adres kunnen meerdere pakjes bezorgd worden'),
+('d', true, 'want P -> F (c) -> A'),
+('e', true, 'want P -> G (5) P -> O (6)'),
+('f', false, 'een bezorger kan best voor meerdere voertuigen gerechtigd zijn?'),
+('g', false, 'bezorger kan best gerechtigd zijn voor twee voertuigen met dezelfde capaciteit'),
+('h', false, 'Niet uit gekomen'),
+('i', false, 'Niet uit gekomen'),
+('j', false, 'Niet uit gekomen');
